0980b8fcbc9f0bf705b2d699c5610f5617302dc0
ina <ina.lai@hyweb.com.tw>
2015-12-11 22:30:31 +0800

    加入GA

---
+++ WebContent/d3/bar/js/index.js
@@ -0,0 +1,6 @@
+var pageData = [
+                {"title":"基本長條圖","url":"base-bar.html"},
+                {"title":"基本長條圖(橫式)","url":"base-bar-horizontal.html"},
+                {"title":"有範圍長條圖","url":"range-bar.html"},
+                {"title":"有範圍長條圖(橫式)","url":"range-bar-horizontal.html"},
+                ];
---
+++ WebContent/d3/bar/js/range-bar-horizontal.js
@@ -0,0 +1,34 @@
+var svg_width = 300;
+var svg_height = 300;
+var padding = 5;
+var dataset = [
+               {"min":10,"max":20},
+   			{"min":25,"max":40},
+   			{"min":30,"max":60},
+   			{"min":18,"max":39},
+   			{"min":3,"max":47}
+   		];
+var svg = d3.select("body")
+	.append("svg")
+	.attr("width",svg_width)
+	.attr("height",svg_height);
+
+svg.selectAll("rect")
+	.data(dataset)
+	.enter()
+	.append("rect")
+	.attr("x",function(d,i){
+		return d.min*4;
+	})
+	.attr("y",function(d,i){
+		return i * (svg_height/dataset.length);
+	})
+	.attr("width",function(d){
+		return  (d.max-d.min)*4;
+	})
+	.attr("height",function(d){
+		return svg_height/dataset.length - padding;
+	})
+	.attr("fill",function(d){
+		 return "rgb(" +  d.max*5 + ",0 ,0 )";
+	});
